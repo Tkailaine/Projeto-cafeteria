41d0866c53223fa3f15d930b9fc465d27a2f09a2
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2024-04-29 13:56:25 -0300

    ajuste loja

--- paginas/002/script.js
+++ paginas/002/script.js
@@ -1,6 +1,8 @@
 var itens = document.getElementById("itens")
 var menu = document.getElementById("divmenu")
 var destaque = document.getElementById("destaque")
+var preco = document.querySelector(".precoc")
+var msg = document.querySelector(".msg")
 
 function fechar(){
     if(itens.style.display!='none'){
@@ -15,5 +17,19 @@ function abrir(){
         menu.style.display='none';
         destaque.style.display='none';
     }
+}
+
+
+function entrou(){
+   if(preco.style.display!='none'){
+    preco.style.display='none';
+    msg.style.display='block';
+}
+}
+function saiu(){
+    if(preco.style.display='none'){
+     preco.style.display='block';
+     msg.style.display='none';
+ }
 }
 
